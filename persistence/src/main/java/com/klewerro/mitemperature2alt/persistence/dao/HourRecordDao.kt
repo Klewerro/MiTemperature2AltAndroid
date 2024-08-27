164e8f3ea5fb548dfe7bb1cecc01829ed8cb5789
Klewerro <pol.szym95@gmail.com>
2024-08-27 16:13:51 +0200

    Fixed issue with saving hourlyRecords.
    Conflict strategy REPLACE set, because it will be used in future anyway

--- persistence/src/main/java/com/klewerro/mitemperature2alt/persistence/dao/HourRecordDao.kt
+++ persistence/src/main/java/com/klewerro/mitemperature2alt/persistence/dao/HourRecordDao.kt
@@ -2,6 +2,7 @@ package com.klewerro.mitemperature2alt.persistence.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.klewerro.mitemperature2alt.persistence.entity.HourRecordEntity
 
@@ -11,6 +12,6 @@ interface HourRecordDao {
     @Query("SELECT * FROM hour_record")
     suspend fun getAll(): List<HourRecordEntity>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertHourRecords(vararg hourRecords: HourRecordEntity)
 }
